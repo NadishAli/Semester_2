7ca951895d8750891fea5bf986cff5894950ecab
NadishAli <nadishali523@gmail.com>
2025-02-22 15:05:37 +0500

    Tasks

---
+++ OOP PD/week 1/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace changeColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name is Nadish.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("My Name is Nadish.");
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("My Name is Nadish.");
+            Console.ForegroundColor= ConsoleColor.White;
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("My Name is Nadish.");
+            Console.BackgroundColor= ConsoleColor.Green;
+            Console.WriteLine("My Name is Nadish.");
+            Console.BackgroundColor=ConsoleColor.Black;
+        }
+    }
+}
---
+++ OOP PD/week 1/GenerateRandomNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateRandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int min = 0;
+            int max = 100;
+            int randomNumber = rnd.Next(min,max);
+            Console.WriteLine("The Random Number is: "+randomNumber);
+        }
+    }
+}
